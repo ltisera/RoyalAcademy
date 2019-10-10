1ac4a3c60863481425d534a9b708d230c6e1a052
enzord07 <enzord07@gmail.com>
2019-10-10 17:33:31 -0300

    Update configs.py

--- DAO/CONFIGS/configs.py
+++ DAO/CONFIGS/configs.py
@@ -7,9 +7,9 @@ def getConfigDB():
     configDB = {}
     if (modo == 'desarrollo'):
         configDB['host'] = 'localhost'
-        configDB['user'] = 'adminODBC'
-        configDB['password'] = '1234'
-        configDB['database'] = 'odbcViajes'
+        configDB['user'] = 'royaladmin'
+        configDB['password'] = 'royaladmin'
+        configDB['database'] = 'royalacademydb'
         configDB['auth_plugin']='mysql_native_password'
-
+        
     return configDB
