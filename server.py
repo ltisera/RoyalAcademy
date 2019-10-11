d36c9c8eeb8a4857f56103dd0db123143d443eae
Pmiranda95 <pablo.miranda.alan95@gmail.com>
2019-10-10 23:55:11 -0300

    login prototipo

--- server.py
+++ server.py
@@ -15,7 +15,7 @@ app = Flask(__name__, static_folder='static', static_url_path='')
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
-    return render_template('index.html')
+    return render_template('login.html')
 
 
 @app.route('/holaEnzo', methods=['GET', 'POST'])
