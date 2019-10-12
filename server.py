82d4ec00ace5796edf9288d782d802a14c762676
Pmiranda95 <pablo.miranda.alan95@gmail.com>
2019-10-12 17:40:49 -0300

    se hizo un modal para login

--- server.py
+++ server.py
@@ -15,7 +15,7 @@ app = Flask(__name__, static_folder='static', static_url_path='')
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
-    return render_template('login.html')
+    return render_template('index.html')
 
 
 @app.route('/alumno', methods=['GET'])
